c2ec01188910740d86846274e73d2abe0734ab7e
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-05 22:38:35 -0700

    toggle modal and clear error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useState} from "react";
+import {FormEvent, useEffect, useState} from "react";
 import './style.css'
 import {IoIosWarning} from "react-icons/io";
 import {BarLoader} from "react-spinners";
@@ -9,6 +9,11 @@ function App() {
     const [data, setData] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+
+    const toggleModal = () => {
+        setErrorMessage('') // clear any error messages
+        setIsModalOpen(!isModalOpen)
+    }
 
     const handlePromise = async (data: string): Promise<void> => {
         return new Promise((resolve, reject) => {
@@ -43,16 +48,16 @@ function App() {
                 <h1>Promise Modal</h1>
                 <p>Current Step: {currentStep}</p>
                 <p>Simple example for showing resolving/rejecting promise in a modal before closing it.</p>
-                <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
+                <button onClick={toggleModal}>Open Modal</button>
             </div>
 
             {isModalOpen && (
                 <>
-                    <div id="overlay" onClick={() => setIsModalOpen(false)}></div>
+                    <div id="overlay" onClick={toggleModal}></div>
                     <div id="modal" onClick={e => e.stopPropagation()}>
                         <header>
                             <h2>Fill Your Information</h2>
-                            <button type="button" onClick={() => setIsModalOpen(false)}>X</button>
+                            <button type="button" onClick={toggleModal}>X</button>
                         </header>
                         {isLoading && <BarLoader speedMultiplier={0.75}/>}
                         <form onSubmit={handleModalSubmit}>
